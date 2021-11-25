dc77e51083523ee9ceb727879fa9a8dd12c2da58
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2021-11-26 01:28:50 +0600

    unknown

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,12 @@
-let mix = require('laravel-mix');
-var path = require('path');
+let mix = require("laravel-mix");
+var path = require("path");
 
-mix.js('src/templates/assets/js/app.js', 'js')
-    .sass('src/templates/assets/scss/main.scss', 'css')
-    .vue({version: 2})
-    .setPublicPath('src/static')
-    .options({
-        runtimeChunkPath: 'static/rr'
-    })
+mix
+  .js("src/templates/assets/js/app.js", "js")
+  .sass("src/templates/assets/scss/main.scss", "css")
+  //   .vue({ version: 2 })
+  //   .react("resources/js/app.jsx", "public/js/app.js")
+  .setPublicPath("src/static")
+  .options({
+    runtimeChunkPath: "static/rr",
+  });
