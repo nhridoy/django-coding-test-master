61d2643a2a224882afe7907e0a7f9c88a055bd70
Sourcery AI <>
2022-08-23 07:24:33 +0000

    'Refactored by Sourcery'

--- src/product/views/variant.py
+++ src/product/views/variant.py
@@ -17,11 +17,13 @@ class VariantView(BaseVariantView, ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        filter_string = {}
         print(self.request.GET)
-        for key in self.request.GET:
-            if self.request.GET.get(key):
-                filter_string[key] = self.request.GET.get(key)
+        filter_string = {
+            key: self.request.GET.get(key)
+            for key in self.request.GET
+            if self.request.GET.get(key)
+        }
+
         return Variant.objects.filter(**filter_string)
 
     def get_context_data(self, **kwargs):
